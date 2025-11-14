9cdaaf7bef3a93b801fe420d0f5da8b66c17e1c2
gaurav101203 <162018942+gaurav101203@users.noreply.github.com>
2025-11-14 14:34:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ async def ask(q: Query):
 
 if __name__ == "__main__":
     import os
-    host = os.getenv("HOST", "127.0.0.1")
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host=host, port=port, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        reload=False
+    )
